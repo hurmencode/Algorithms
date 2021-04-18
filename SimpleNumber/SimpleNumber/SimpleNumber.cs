22fa608c0134483fccec59aa4f4a7b6f1ac86e45
hurma <karaulnihiv@gmail.com>
2021-04-19 00:01:30 +0300

    Первое задание

---
+++ SimpleNumber/SimpleNumber/SimpleNumber.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Lesson_1
+{
+    class SimpleNumber
+    {
+        static void Main(string[] args)
+        {
+            simple(1);
+            simple(3);
+            simple(6);
+        }
+
+        static void simple(int number)
+        {
+            int d = 0;
+            int i = 2;
+
+            while (i < number)
+            {
+                if ((number % i) == 0)
+                {
+                    d++;
+                }
+
+                i++;
+            }
+
+            if (d == 0)
+            {
+                Console.WriteLine("Простое");
+            }
+            else
+            {
+                Console.WriteLine("Не простое");
+            }
+            
+        }
+
+       
+    }
+
+}
